0e35b8e92efaddd0b810e81c6c16c1b63fec062c
Vignesh100799 <baluvignesh422@gmail.com>
2023-12-26 17:57:32 +0530

    First commit

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
           <span className="ms-2 text-light">The React News</span>
         </a>
        <button
-          className="navbar-toggler bg-light"
+          className="navbar-toggler align-content-center bg-light"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
@@ -26,7 +26,7 @@ const Navbar = () => {
           <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse nav-scroller navbar-collapse justify-content-end" id="navbarNav">
-        <ul className="navbar-nav nav-underline ">
+        <ul className="navbar-nav nav-underline text-center">
               <li className="nav-item">
                 <a className="nav-link link-body-emphasis active text-light" aria-current="page" href="#">
                   Home
