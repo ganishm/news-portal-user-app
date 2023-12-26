4d15dfc70d116e5a7262778bc12cc0e685e61ab0
Vignesh100799 <baluvignesh422@gmail.com>
2023-12-26 19:14:41 +0530

    Deleted png files

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import logo from "./logo.png";
 import vrlogo from './final.png'
+
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-dark text-light sticky-top">
